167b4017b1211dcaf14b802292f6f6a16ae056b0
Lewis Reid <lreidwilliams@gmail.com>
2021-02-11 01:15:27 +1000

    raise error when using smtp and email is not provided

--- lib/delivery/smtp.rb
+++ lib/delivery/smtp.rb
@@ -2,6 +2,8 @@ module Delivery
   class Smtp
     def deliver(message, to:)
       to.fill_out do |details|
+        raise 'An email address is required' unless details.key?(:email)
+
         puts "Send #{message} to #{details}"
       end
     end
